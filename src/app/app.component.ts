92e976ddb5df2583b69149b2df7f74a159824371
Vitorio Mincarone <vitorio.tfm@gmail.com>
2021-12-26 09:45:31 -0300

    Angular - The Complete Guide - lecture #86

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng4-complete-guide';
+
+  selectedRecipes = false;
+
+  onNavigate(feature: string) {
+    this.selectedRecipes = feature === 'recipes';
+  }
 }
