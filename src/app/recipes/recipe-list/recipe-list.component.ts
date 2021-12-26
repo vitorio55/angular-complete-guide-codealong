dc62632e6f4396da4a6fce44b40eac628f5a78ef
Vitorio Mincarone <vitorio.tfm@gmail.com>
2021-12-26 10:29:59 -0300

    Angular - The Complete Guide - lecture #88

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,15 +8,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
+      '2x tomates; 1x carrot',
       'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg'
     ),
     new Recipe(
       'This Is Another Recipe',
       'Testing again with another recipe',
+      '1x tea-spoon of salt; 500ml of water',
       'https://www.saveur.com/uploads/2020/11/20/Y7RZPFZEERAZVHJ2VHC2RXMEEY.jpg?quality=85&width=540'
     ),
   ];
@@ -24,6 +28,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
