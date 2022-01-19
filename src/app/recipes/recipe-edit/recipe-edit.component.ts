cd2f79177dad0a2747cc05f06b96a89352a75ea3
Vitorio Mincarone <vitorio.tfm@gmail.com>
2022-01-19 08:47:01 -0300

    Angular - The Complete Guide - lecture #231

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../services/recipe.service';
 
@@ -27,6 +27,11 @@ export class RecipeEditComponent implements OnInit {
       );
   }
 
+  getFormControls(): AbstractControl[] {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+    // return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
   onSubmit() {
     console.log(this.recipeForm);
   }
@@ -35,19 +40,30 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount),
+            }),
+          );
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
       'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients,
     });
   }
-
 }
