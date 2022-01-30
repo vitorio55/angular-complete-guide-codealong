986c55379d4f59c9af18ee9cbd8b199ac8c85cd7
Vitorio Mincarone <vitorio.tfm@gmail.com>
2022-01-29 21:27:19 -0300

    Angular - The Complete Guide - lecture #314 - Creating placeholder directive for programmatic components

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
+import { AlertComponent } from '../shared/alert/alert.component';
 
 import { AuthResponseData } from './auth-response-data.model';
 import { AuthService } from './auth.service';
@@ -20,13 +21,19 @@ export class AuthComponent implements OnInit {
   success = false;
   error: string = null;
 
-  constructor(private authService: AuthService,
-              private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
 
   ngOnInit() {
     this.authForm = new FormGroup({
-      'email': new FormControl('', [Validators.required, Validators.email]),
-      'password': new FormControl('', [Validators.required, Validators.minLength(6)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     });
   }
 
@@ -58,20 +65,30 @@ export class AuthComponent implements OnInit {
     this.error = null;
   }
 
+  private showErrorAlert(message: string) {
+    // Programmatic component error modal approach
+    const alertComponentFactory =
+      this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+  }
+
   private handleAuthObservable(obs: Observable<AuthResponseData>) {
-    obs.pipe(
-      finalize(() => {
-        this.isLoading = false;
-        this.success = this.error ? false : true;
-      }),
-    )
-    .subscribe(responseData => {
-      console.log(responseData);
-      this.router.navigate(['/recipes']);
-    },
-    errorMessage => {
-      this.error = errorMessage;
-      console.log(errorMessage);
-    });
+    obs
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          this.success = this.error ? false : true;
+        })
+      )
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+          this.router.navigate(['/recipes']);
+        },
+        (errorMessage) => {
+          this.error = errorMessage;
+          this.showErrorAlert(errorMessage);
+          console.log(errorMessage);
+        }
+      );
   }
 }
