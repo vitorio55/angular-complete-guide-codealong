58bf523e786945ef0f272a5c9763a270eae72da7
Vitorio Mincarone <vitorio.tfm@gmail.com>
2022-01-30 09:51:40 -0300

    Angular - The Complete Guide - lecture #325 - Migrating recipes routes to recipes feature module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipesComponent } from './recipes.component';
 
 @NgModule({
@@ -26,6 +27,7 @@ import { RecipesComponent } from './recipes.component';
     // the CommomModule
     CommonModule,
     ReactiveFormsModule,
+    RecipesRoutingModule,
   ],
   exports: [
     RecipesComponent,
