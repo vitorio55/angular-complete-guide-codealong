333e50481acd7cd8ef2de624bfb851ea6f06744e
Vitorio Mincarone <vitorio.tfm@gmail.com>
2022-01-31 08:43:25 -0300

    Angular - The Complete Guide - lecture #334 - Adding preloading strategy for loading lazy-loaded modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -27,7 +27,19 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    /*
+      ● Without lazy loading the whole app would have to be loaded in one go, and the user wouldn't
+        see anything until the code for the whole app (all "pages") is ready.
+      ● With lazy loading we just load the first module, so that the user can see the initial page/s
+        very quickly. But without preloading the user will then have to wait for the loading of
+        additional modules each time he wants to move to different "subpages".
+      ● PreloadAllModules:
+        To avoid the above behavior, the other modules will be loaded in the background, as soon as
+        the first module is loaded and displayed.
+    */
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
