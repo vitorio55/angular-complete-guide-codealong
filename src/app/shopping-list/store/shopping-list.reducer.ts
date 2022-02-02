77e3ba5a856952d28fbf2475773c48d33b550b56
Vitorio Mincarone <vitorio.tfm@gmail.com>
2022-02-02 15:18:45 -0300

    Angular - The Complete Guide - lecture #350 - Moving store related code to store dir inside shopping-list dir - Creating shopping-list actions file for store logic

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,6 +1,7 @@
 import { Action } from '@ngrx/store';
 
-import { Ingredient } from "../shared/ingredient.model";
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ADD_INGREDIENT } from './shopping-list.actions';
 
 const initialState = {
   ingredients: [
@@ -13,7 +14,7 @@ const initialState = {
 
 export function shoppingListReducer(state = initialState, action: Action) {
   switch (action.type) {
-    case 'ADD_INGREDIENT':
+    case ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [
